3e6ab514e0c56becdafaa4c8bc82d1282057e1b7
Michael Harms <michael.harms6010@gmail.com>
2020-05-06 14:19:55 -0500

    builds login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -19,20 +19,9 @@ router.post('/register',  (req, res) => {
 
 router.post('/login', (req, res) => {
   let {username, password} = req.body;
-
-  Users.findBy({username})
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-       const token = generateToken(user) 
-       delete user.password;
-        res.status(200).json({
-         token
-        });
-      } else {
-        res.status(401).json({message: "Invalid Credentials"})
-      }
-    })
+  collection.find({username})
+    .then(res => res.status(201).json(res))
+    .catch(err => res.status(500).json(err))
 
 });
 
